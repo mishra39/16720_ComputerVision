1fa0522aa549f241c24d91dbeb3d296f508bec86
mishra39 <akshitmishra1995@gmail.com>
2020-04-16 13:21:55 -0400

    letter recognition done

--- hw5/hw5/python/q4.py
+++ hw5/hw5/python/q4.py
@@ -13,12 +13,21 @@ import skimage.segmentation
 def findLetters(image):
     bboxes = []
     bw = None
+
     # insert processing in here
-    # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes 
+    # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes
     # this can be 10 to 15 lines of code using skimage functions
 
-    ##########################
-    ##### your code here #####
-    ##########################
+    im_denoise = skimage.restoration.denoise_wavelet(image,multichannel=True) #denoise
+    im_grey = skimage.color.rgb2gray(im_denoise) # grayscale
+    im_thresh = skimage.filters.threshold_otsu(im_grey) # threshold
+    bw = skimage.morphology.closing(im_grey < im_thresh, skimage.morphology.square(5))
+    label_image = skimage.measure.label(bw,connectivity=2) # What does this do?
+    regions = skimage.measure.regionprops(label_image)
 
+    area_mean = 0
+    area_mean = sum([i.area for i in regions])/len(regions)
+
+    bboxes = [i.bbox for i in regions if i.area > area_mean/2]
+    bw = (~bw).astype(np.float)
     return bboxes, bw
--- hw5/hw5/python/run_q4.py
+++ hw5/hw5/python/run_q4.py
@@ -19,7 +19,6 @@ from q4 import *
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
-
 for img in os.listdir('../images'):
     im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
     bboxes, bw = findLetters(im1)
@@ -31,26 +30,74 @@ for img in os.listdir('../images'):
                                 fill=False, edgecolor='red', linewidth=2)
         plt.gca().add_patch(rect)
     plt.show()
+
     # find the rows using..RANSAC, counting, clustering, etc.
-    ##########################
-    ##### your code here #####
-    ##########################
+    y_vals = [bbox[2]-bbox[0] for bbox in bboxes]
+    avg_h = sum(y_vals)/len(y_vals) # average Height of boxes
+
+    centers = [[(bbox[3] + bbox[1])//2, (bbox[2] + bbox[0])//2, bbox[3]-bbox[1],bbox[2]-bbox[0]] for bbox in bboxes]
+    centers.sort(key = lambda i:i[1]) # Sort based on x1 values
+    x_init = centers[0][1]
+
+    row_val = []
+    row_list= []
+
+    for coord in centers:
+        if coord[1] > x_init + avg_h:
+            row_val = sorted(row_val, key=lambda coord: coord[0]) #sort based on y values
+            row_list.append(row_val)
+            x_init = coord[1]
+            row_val = [coord]
+
+        else:
+            row_val.append(coord)
 
 
-    # crop the bounding boxes
+    row_val = sorted(row_val, key=lambda coord:coord[0]) #sort based on y values
+
+    row_list.append(row_val) # coordinates of rectangle rows
+
+    im_mat = []
+
+    for row in row_list:
+        im_row = []
+
+        for x_cord, y_cord, w, h in row:
+            im_crop = bw[y_cord - h//2 : y_cord + h//2, x_cord - w//2 : x_cord + w//2]  # crop the bounding boxes
+
     # note.. before you flatten, transpose the image (that's how the dataset is!)
     # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
-    ##########################
-    ##### your code here #####
-    ##########################
-    
+            # pad_h = (np.abs(h-w)//2) +(np.max([h,w])//20)
+            if h > w:
+                pad_h = h//20
+                pad_w = (h-w)//2 + pad_h
+
+            else:
+                pad_w = w//20
+                pad_h = (w-h)//2 + pad_h
+
+            im_crop = np.pad(im_crop,((pad_h, pad_h),(pad_w,pad_w)),'constant',constant_values=(1,1))
+            im_crop = skimage.transform.resize(im_crop, (32,32))
+            im_crop = (skimage.morphology.erosion(im_crop)).T
+            crop_flat = im_crop.flatten()
+            im_row.append(crop_flat)
+
+        im_arr = np.asarray(im_row)
+        im_mat.append(im_arr)
+
+
     # load the weights
     # run the crops through your neural network and print them out
     import pickle
     import string
     letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
     params = pickle.load(open('q3_weights.pickle','rb'))
-    ##########################
-    ##### your code here #####
-    ##########################
-    
+
+    for im_arr in im_mat:
+        h1 = forward(im_arr,params,'layer1')
+        probs = forward(h1,params,'output',softmax)
+        pred_loc = np.argmax(probs, axis=1)
+        pred_val = ''
+        for i in pred_loc:
+            pred_val +=  letters[i]
+        print(pred_val)
